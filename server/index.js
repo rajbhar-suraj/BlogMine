4d8fc2bdab74a3539006e9e6440551fb4e299bab
rajbhar-suraj <suraj.rajbhar.tech@gmail.com>
2025-06-03 21:34:11 +0530

    fifth commit

--- server/index.js
+++ server/index.js
@@ -11,22 +11,23 @@ const connectDb = require('./config/db')
 dotenv.config()
 connectDb()
 const app = express()
-app.use(express.urlencoded({ extended: true })); 
-app.use(cookieParser())
+
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://blog-mine-three.vercel.app'],
-    credentials : true
+    origin: ['http://localhost:5173', 'https://blog-mine-three.vercel.app', 'https://blog-mine-90v7i5g8e-suraj-rajbhars-projects.vercel.app', 'https://blog-mine-90v7i5g8e-suraj-rajbhars-projects.vercel.app'],
+    credentials: true
 }))
-app.use(express.json()); // ⬅️ This is required for JSON body parsing
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 
 app.use('/uploads', express.static('uploads'));
 
-app.use('/auth',UserRoute)
-app.use('/auth/blog',BlogRoute)
-app.use('/auth/blog/comment',ReportRoute)
+app.use('/auth', UserRoute)
+app.use('/auth/blog', BlogRoute)
+app.use('/auth/blog/comment', ReportRoute)
 
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>console.log(`Server is running on ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
